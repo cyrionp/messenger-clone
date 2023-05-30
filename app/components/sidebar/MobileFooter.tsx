1b2db2714cb14950b44d172afeea6a33a777ad68
Abdulkadir Durmaz <43341682+cyrionp@users.noreply.github.com>
2023-05-30 11:14:10 +0300

    sidebar, navigation, and layout is finished

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -12,32 +12,32 @@ const MobileFooter = () => {
 		return null;
 	}
 
-	return(
-        <div
-            className='
-                fixed
-                justify-between
-                w-full
-                bottom-0
-                z-40
-                flex
-                items-center
-                bg-white
-                border-t-[1px]
-                lg-hidden
-            '
-        >
-            {routes.map((route)=>(
-                <MobileItem
-                    key={route.href}
-                    href={route.href}
-                    active={route.active}
-                    icon={route.icon}
-                    onClick={route.onClick}
-                />
-            ))}
-        </div>
-    );
+	return (
+		<div
+		    className='
+		        fixed
+		        justify-between
+		        w-full
+		        bottom-0
+		        z-40
+		        flex
+		        items-center
+		        bg-white
+		        border-t-[1px]
+				lg:hidden
+		    '
+		>
+		    {routes.map((route)=>(
+		        <MobileItem
+		            key={route.href}
+		            href={route.href}
+		            active={route.active}
+		            icon={route.icon}
+		            onClick={route.onClick}
+		        />
+		    ))}
+		</div>
+	);
 };
 
 export default MobileFooter;
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-	title: 'Messenger Clone',
+	title: 'Messenger',
 	description: 'Messenger Clone',
 };
 
